1ac499202ccf15775359cf3365b649f940b01663
Irina Karmatskikh <irina_karmatskikh@mail.ru>
2021-10-07 14:30:15 +0300

    Создание базы данных
    SQLite3.
    3 таблицы: статья, автор и  статья-автор для разделения связи много ко многим

--- load_crossref.py
+++ load_crossref.py
@@ -11,11 +11,13 @@ import time
 import logging
 import os
 import gzip
-import io
+import sqlite3
+from sqlite3 import Error
 
 CROSSREF_ENDPOINT = 'https://api.crossref.org'
 CROSSREF_MAILTO = 'serg@msu.ru'
 CROSSREF_DIRSAVE = r'C:\Users\Ирина\Documents\Search (for medic)\JSON\ '
+DATABASE_DIRSAVE = r'C:\Users\Ирина\Documents\Search (for medic)\DATABASE\db_python_app.sqlite'
 
 logging.basicConfig(filename='doi_fetcher.log', level=logging.INFO)
 
@@ -57,12 +59,13 @@ def fetch_url(url, params=None, data=None):
         try:
             with urllib.request.urlopen(request) as response:
                 # response -  class http.client.HTTPResponse(sock, debuglevel=0, method=None, url=None)
-                # Class whose instances are returned upon successful connection. Not instantiated directly by user.
+                # Class whose instances are returned upon save_itemsessful connection. Not instantiated directly by user.
                 message_bytes = response.read() #read?
                 message = message_bytes.decode('utf-8')
                 print("Success! 20 new downloads")
                 return json.loads(message) #json в объект питона
         except urllib.error.URLError as err:
+            print(err)
             logging.warning('Fetch error: %s', err)
             logging.warning('Complete URL: %s', actual_url)
             logging.warning('Waiting for %s seconds before next download attempt.', timeout)
@@ -133,7 +136,6 @@ class CrossrefFetcher:
 class CrossrefSave:
     def save_items(self, json_response, number_dirs, start_date):
         items = json_response.get('message', {}).get('items')
-        file_name = ""
         data = start_date.strftime('%d-%m-%Y')
         dir_name = str(number_dirs)
         os.makedirs(CROSSREF_DIRSAVE + data + r'\ ' + dir_name, exist_ok = True)
@@ -144,6 +146,60 @@ class CrossrefSave:
                 with gzip.GzipFile(full_dir_name, 'w') as outfile:
                     outfile.write(json.dumps(items[i]).encode('utf-8'))
 
+class Database:
+
+    def create_connection(self, path):
+        connection = None
+        try:
+            connection = sqlite3.connect(path)
+            print("Connection to SQLite DB successful")
+        except Error as e:
+            print(f"The error '{e}' occurred")
+        return connection
+
+    def execute_query(self, connection, query):
+        cursor = connection.cursor()
+        try:
+            cursor.execute(query)
+            connection.commit()
+            print("Query executed successfully")
+        except Error as e:
+            print(f"The error '{e}' occurred")
+
+    def create_table(self):
+        connection = self.create_connection(DATABASE_DIRSAVE)
+        create_pragma = """
+        PRAGMA foreign_keys = on;
+        """
+        create_article_table = """
+        CREATE TABLE IF NOT EXISTS article(
+         id INTEGER PRIMARY KEY AUTOINCREMENT,
+         doi INTEGER,
+         title TEXT NOT NULL,
+         data TEXT 
+         address TEXT
+        );
+        """
+        create_auth_table = """ 
+        CREATE TABLE IF NOT EXISTS auth(
+         id INTEGER PRIMARY KEY AUTOINCREMENT,
+         name TEXT NOT NULL
+        );
+        """
+
+        create_auth_article_table = """ 
+        CREATE TABLE IF NOT EXISTS auth_article (
+         auth_id INTEGER NOT NULL,
+         article_id INTEGER NOT NULL,
+         FOREIGN KEY (auth_id) REFERENCES auth(id)
+         FOREIGN KEY (article_id) REFERENCES article(id)
+        );
+        """
+        self.execute_query(connection, create_pragma)
+        self.execute_query(connection, create_article_table)
+        self.execute_query(connection, create_auth_table)
+        self.execute_query(connection, create_auth_article_table)
+
 
 def main():
     """Загрузка за предыдущие сутки."""
@@ -153,6 +209,8 @@ def main():
     today = date.today()
     # создаем элемент класса CrossrefFetcher
     fetcher = CrossrefFetcher()
+    database = Database()
+    database.create_table()
     for delay in range(number_of_days_to_fetch, 0, -1):
         yesterday = today - timedelta(delay)
         the_day_before_yesterday = yesterday - timedelta(1)
@@ -160,7 +218,7 @@ def main():
 
         data = the_day_before_yesterday.strftime('%d-%m-%Y')
         newDirName = data
-        folder = os.makedirs(CROSSREF_DIRSAVE + newDirName, exist_ok=True)
+        folder = os.makedirs(r'C:\Users\Ирина\Documents\Search (for medic)\JSON\ ' + newDirName, exist_ok=True)
         #Скачевает все записи, начиная с даты start_date и заканчивая end_date
 
         fetcher.retrieve_new_dois(start_date=the_day_before_yesterday, end_date=the_day_before_yesterday)
